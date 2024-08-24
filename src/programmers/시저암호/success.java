6b5870abcf4d07c8c1755e2834e797500d1278f5
seoyeoning <minalunpark@naver.com>
2024-08-24 08:26:56 +0200

    프로그래머스-시저 암호 진행중

---
+++ src/programmers/시저암호/success.java
@@ -0,0 +1,32 @@
+package programmers.시저암호;
+import java.util.*;
+
+public class success {
+
+    public String solution(String s, int n) {
+        String answer = "";
+
+        // String[] upper = {"A","B","C","D","E","F","G","H","I","J","K",
+        //                  "L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"};
+        // String[] lower = {"a","b","c","d","e","f","g","h","i","j","k",
+        //                  "l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"};
+
+        char[] upper = {'A','B','C','D','E','F','G','H','I','J','K',
+                'L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'};
+        char[] lower = {'a','b','c','d','e','f','g','h','i','j','k',
+                'l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
+
+        int s_len = s.length();
+
+        System.out.println(s_len);
+
+        if (s_len != 0) {
+            for (int i=0; i < s_len; i++) {
+
+            }
+        }
+
+        return answer;
+    }
+
+}
