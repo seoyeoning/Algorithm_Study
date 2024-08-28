fb5a4032bed48a341a8f659533fb2f5ccba708e6
seoyeoning <minalunpark@naver.com>
2024-08-28 22:47:13 +0200

    프로그래머스-달리기 경주 진행중

---
+++ src/programmers/달리기경주/success.java
@@ -0,0 +1,31 @@
+package programmers.달리기경주;
+import java.util.*;
+
+public class success {
+
+    public String[] solution(String[] players, String[] callings) {
+        // String[] answer = {};
+
+        System.out.println(Arrays.asList(players).indexOf("soe"));
+
+        System.out.println(callings.length);
+
+        int win_index = -1;
+        String loser = "";
+
+        for (int i=0; i < callings.length; i++) {
+
+            win_index = Arrays.asList(players).indexOf(callings[i]);
+            // System.out.println(call_index);
+
+            loser = players[win_index-1];
+
+            players[win_index] = loser;
+            players[win_index-1] = callings[i];
+
+        }
+
+        return players;
+    }
+
+}
