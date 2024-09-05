82c49947b75f1160c204ce19b96315c7f56eabde
seoyeoning <minalunpark@naver.com>
2024-09-05 17:35:14 +0900

    프로그래머스-햄버거 만들기 해결

--- src/programmers/햄버거만들기/success.java
+++ src/programmers/햄버거만들기/success.java
@@ -1,9 +1,39 @@
 package programmers.햄버거만들기;
 
+import java.util.*;
+// import java.util.stream.Collectors;
+
 public class success {
 
     public int solution(int[] ingredient) {
         int answer = 0;
+
+        Stack<Integer> st = new Stack<Integer>();
+
+        for (int in : ingredient) {
+
+            st.push(in);
+
+            if (st.size() >= 4) {
+                if (st.get(st.size() - 4) == 1
+                        && st.get(st.size() - 3) == 2
+                        && st.get(st.size() - 2) == 3
+                        && st.get(st.size() - 1) == 1) {
+
+                    answer++;
+                    st.pop();
+                    st.pop();
+                    st.pop();
+                    st.pop();
+                }
+            }
+
+        }
+
+        return answer;
+    }
+
+}
 
 //         List<Integer> hamburger =
 //             Arrays.stream(ingredient)
@@ -11,8 +41,3 @@ public class success {
 //             .collect(Collectors.toList());
 
 //         System.out.println(hamburger.size());
-
-        return answer;
-    }
-
-}
