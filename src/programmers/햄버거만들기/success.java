fbf274e6dd54a3354c96596db6c230276bdb1cf6
seoyeoning <minalunpark@naver.com>
2024-09-05 17:18:04 +0900

    프로그래머스-햄버거 만들기 해결중

--- src/programmers/햄버거만들기/success.java
+++ src/programmers/햄버거만들기/success.java
@@ -1,4 +1,18 @@
 package programmers.햄버거만들기;
 
 public class success {
+
+    public int solution(int[] ingredient) {
+        int answer = 0;
+
+//         List<Integer> hamburger =
+//             Arrays.stream(ingredient)
+//             .boxed()
+//             .collect(Collectors.toList());
+
+//         System.out.println(hamburger.size());
+
+        return answer;
+    }
+
 }
