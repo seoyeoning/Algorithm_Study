8352bc7f3452c667d209fd73c92950f509e6d84a
seoyeoning <minalunpark@naver.com>
2024-11-01 21:48:48 +0900

    이테코 - 1이 될 때까지

---
+++ src/이코테/_1이될때까지/Main.java
@@ -0,0 +1,38 @@
+package 이코테._1이될때까지;
+
+import java.io.*;
+import java.util.*;
+
+// n이 k의 배수가 될때까지 1을 빼줘야 하는 비효율적인 코드
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+
+        int result = 0;
+
+        while (true) {
+
+            if (n % k == 0) {
+                n /= k;
+            } else {
+                n--;
+            }
+            result++;
+            if (n == 1) {
+                break;
+            }
+
+        }
+
+        System.out.println(result);
+
+
+    }
+
+}
