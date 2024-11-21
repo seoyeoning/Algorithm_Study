7245125bb9db99ec3ea08a0feaafafe2f56d76fd
seoyeoning <minalunpark@naver.com>
2024-11-21 23:53:26 +0900

    이테코 이진탐색 - 부품 찾기

--- src/이코테/이진탐색/부품찾기/Main.java
+++ src/이코테/이진탐색/부품찾기/Main.java
@@ -1,36 +1,66 @@
 package 이코테.이진탐색.부품찾기;
 
-import java.util.ArrayList;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Arrays;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Queue;
+import java.util.StringTokenizer;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
-        float f = 4.4F;
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
 
-        int i = (int) f;
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int[] list = new int[N];
 
-        int j = Math.round(f);
+        for (int i = 0; i < N; i++) {
+            list[i] = Integer.parseInt(st.nextToken());
+        }
 
-        System.out.println(i);
-        System.out.println(j);
+        int M = Integer.parseInt(br.readLine());
 
-        Queue<Integer> queue = new LinkedList<>();
-        queue.offer(1);
-        queue.offer(2);
+        st = new StringTokenizer(br.readLine());
 
-        Integer[] list = queue.toArray(new Integer[0]);
+        int[] order_list = new int[M];
 
-        System.out.println(Arrays.toString(list));
+        for (int i = 0; i < M; i++) {
+            order_list[i] = Integer.parseInt(st.nextToken());
+        }
 
-//        List<Integer> list1 = Arrays.asList(list);
-        List<Integer> list1 = new ArrayList<>(Arrays.asList(list));
-        list1.add(4);
-        System.out.println(list1);
+        // list 오름차순 정렬
+        Arrays.sort(list);
+
+        for (int target : order_list) {
+            boolean result = binary_search(list, target, 0, list.length - 1);
+
+            if (result) {
+                System.out.print("yes ");
+            } else {
+                System.out.print("no ");
+            }
+        }
+
+    }
+
+    // 재귀 이진탐색
+    public static boolean binary_search(int[] list, int target, int start_index, int end_index) {
+
+        if (start_index > end_index) { // 찾는 값이 없음
+            return false;
+        }
+
+        int mid_index = (start_index + end_index) / 2;
+
+        if (target == list[mid_index]) {
+            return true;
+        } else if (target > list[mid_index]) {
+            return binary_search(list, target, mid_index + 1, end_index);
+        } else {
+            return binary_search(list, target, start_index, mid_index - 1);
+        }
 
     }
 
