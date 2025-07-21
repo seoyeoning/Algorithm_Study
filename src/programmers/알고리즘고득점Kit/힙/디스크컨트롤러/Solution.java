f0ed0f80fa20e43df35b2021c9da296ba79ce907
seoyeoning <minalunpark@naver.com>
2025-07-21 23:30:44 +0900

    프로그래머스 고득점 kit 힙 - 디스크 컨트롤러

---
+++ src/programmers/알고리즘고득점Kit/힙/디스크컨트롤러/Solution.java
@@ -0,0 +1,10 @@
+package programmers.알고리즘고득점Kit.힙.디스크컨트롤러;
+
+import java.util.*;
+
+class Solution {
+    public int solution(int[][] jobs) {
+        int answer = 0;
+        return answer;
+    }
+}
