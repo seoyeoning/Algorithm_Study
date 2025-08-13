d532043c54675d96f4019fc8d613e00f1ed886d5
seoyeoning <minalunpark@naver.com>
2025-08-13 22:51:25 +0900

    프로그래머스 고득점 kit DFS/BFS - 단어 변환

---
+++ src/programmers/알고리즘고득점Kit/DFS_BFS/단어변환/Solution.java
@@ -0,0 +1,89 @@
+package programmers.알고리즘고득점Kit.DFS_BFS.단어변환;
+
+import java.util.*;
+
+class Solution {
+
+    static int result = 50;
+    static int count = 0;
+
+    public int solution(String begin, String target, String[] words) {
+        int answer = 0;
+
+        int begin_size = begin.length();
+        boolean[] visited = new boolean[words.length];
+
+        for(int i=0; i < begin_size; i++) {
+            dfs(i,begin,target,words,visited);
+        }
+
+        if(result == 50) {
+            return 0;
+        } else {
+            return result;
+        }
+    }
+
+    static public void dfs(int index, String word, String target, String[] words, boolean[] visited) {
+        if(word.equals(target)) {
+            result = Math.min(result,count);
+            return;
+        }
+
+        if (index == 0) {
+            String index_first = word.substring(1);
+            for(int i=0; i < words.length; i++) {
+                if(words[i].substring(1).equals(index_first) && !visited[i]) {
+                    count++;
+                    visited[i] = true;
+                    for(int j=0; j < target.length(); j++) {
+                        dfs(j,words[i],target,words,visited);
+                        if(words[i].equals(target)) {
+                            visited[i] = false;
+                            count--;
+                            return;
+                        }
+                    }
+                    visited[i]=false;
+                }
+            }
+        } else if (index == target.length()-1) {
+            String index_last = word.substring(0,target.length()-1);
+            for(int i=0; i < words.length; i++) {
+                if (words[i].substring(0,target.length()-1).equals(index_last) && !visited[i]) {
+                    count++;
+                    visited[i] = true;
+                    for(int j=0; j < target.length(); j++) {
+                        dfs(j,words[i],target,words,visited);
+                        if(words[i].equals(target)) {
+                            visited[i] = false;
+                            count--;
+                            return;
+                        }
+                    }
+                    visited[i]=false;
+                }
+            }
+            count--;
+        } else {
+            String index_middle = word.substring(0,index) + word.substring(index+1);
+            for(int i=0; i < words.length; i++) {
+                if ((words[i].substring(0,index) + words[i].substring(index+1)).equals(index_middle) && !visited[i]) {
+                    count++;
+                    visited[i] = true;
+                    for(int j=0; j < target.length(); j++) {
+                        dfs(j,words[i],target,words,visited);
+                        if(words[i].equals(target)) {
+                            visited[i] = false;
+                            count--;
+                            return;
+                        }
+                    }
+                    visited[i]=false;
+                }
+            }
+        }
+    }
+}
+
+
